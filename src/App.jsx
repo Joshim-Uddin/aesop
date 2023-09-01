3646bd3edfbfedb404e38c76fa78e65748df945f
Joshim-Uddin <mju881@gmail.com>
2023-09-01 10:22:36 +0600

    initial setup done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Main from "./Layout/Main"
+
+function App() {
+  
+  return (
+    <>
+    <Main />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TopBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TopBar = () => {
+    return (
+        <div className='bg-[#C2C2AF] flex gap-3 justify-center py-2'>
+            <h6>Trained Aesop consultations are available to provide bespoke skin care advise.</h6>
+            <a href="#" className='font-semibold hover:underline'>Book a video consultation</a>
+        </div>
+    );
+};
+
+export default TopBar;
